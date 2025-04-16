b88ff89b8343b1d518a775d2338a302c66b2f79c
trantrungnghia414 <nghiatrandtnt2018@gmail.com>
2025-04-16 16:01:54 +0700

    update nav

--- src/app/facilities/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function facilitie() {
-    return <div>facilitie</div>;
-}
--- src/app/news/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function news() {
-  return (
-    <div>news</div>
-  )
-}

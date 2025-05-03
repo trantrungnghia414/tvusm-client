dbb056ef91b551d985ad8a29ea3592c7156db35c
trantrungnghia414 <nghiatrandtnt2018@gmail.com>
2025-05-03 20:39:24 +0700

    update UI admin-user

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -4,7 +4,7 @@ export default function AdminPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-2xl font-bold mb-4">User Costumer</h1>
-            <p className="text-gray-700">Welcome to the User Costumer!!!</p>
+            <p className="text-gray-700">Welcome to the User Costumer!!</p>
         </div>
     );
 }

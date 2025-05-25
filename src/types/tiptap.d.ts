e19ce40caf352bc501561bf6b97fe8048783179c
trantrungnghia414 <nghiatrandtnt2018@gmail.com>
2025-05-25 11:18:31 +0700

    create ui events manager

---
+++ src/types/tiptap.d.ts
@@ -0,0 +1,7 @@
+declare module "@tiptap/react";
+declare module "@tiptap/starter-kit";
+declare module "@tiptap/extension-placeholder";
+declare module "@tiptap/extension-underline";
+declare module "@tiptap/extension-image";
+declare module "@tiptap/extension-link";
+declare module "@tiptap/extension-text-align";
